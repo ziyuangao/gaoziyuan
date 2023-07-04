7abb8c67241d4f9625308a571387baecbc7702b6
ziyuan_gao@163.com <ziyuan_gao@163.com>
2023-07-04 10:11:39 +0800

    update

--- src/components/common/userInfo.vue
+++ src/components/common/userInfo.vue
@@ -26,8 +26,8 @@ export default{
                 {id:1,src:'http://gaoziyuan.oss-cn-beijing.aliyuncs.com/img/ai1.jpg'},
                 {id:2,src:'http://gaoziyuan.oss-cn-beijing.aliyuncs.com/img/ai2.jfif'},
                 {id:3,src:"http://gaoziyuan.oss-cn-beijing.aliyuncs.com/img/avatar1.jpg"},
-                // {id:4,src:"http://gaoziyuan.oss-cn-beijing.aliyuncs.com/img/avatar2.jpg"},
-                {id:5,src:"http://gaoziyuan.oss-cn-beijing.aliyuncs.com/img/avatar3.jpg"},
+                {id:4,src:"http://gaoziyuan.oss-cn-beijing.aliyuncs.com/img/avatar2.jpg"},
+                // {id:5,src:"http://gaoziyuan.oss-cn-beijing.aliyuncs.com/img/avatar3.jpg"},
                 // {id:6,src:"http://gaoziyuan.oss-cn-beijing.aliyuncs.com/img/avatar4.jpg"},
             ]
         })
