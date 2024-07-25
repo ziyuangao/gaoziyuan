fa6dd69635ed73541ac249c02e47f682ed9a589a
ziyuan_gao <ziiyyuan_gao@163.com>
2024-07-25 16:29:16 +0800

    请求协议由http改为https

--- src/components/common/userInfo.vue
+++ src/components/common/userInfo.vue
@@ -23,10 +23,10 @@ export default{
                 indicatorPosition:'none'
             },
             avatarArr:[
-                {id:1,src:'http://gaoziyuan.oss-cn-beijing.aliyuncs.com/img/ai1.jpg'},
-                {id:2,src:'http://gaoziyuan.oss-cn-beijing.aliyuncs.com/img/ai2.jfif'},
-                {id:3,src:"http://gaoziyuan.oss-cn-beijing.aliyuncs.com/img/avatar1.jpg"},
-                {id:4,src:"http://gaoziyuan.oss-cn-beijing.aliyuncs.com/img/avatar2.jpg"},
+                {id:1,src:'https://gaoziyuan.oss-cn-beijing.aliyuncs.com/img/ai1.jpg'},
+                {id:2,src:'https://gaoziyuan.oss-cn-beijing.aliyuncs.com/img/ai2.jfif'},
+                {id:3,src:"https://gaoziyuan.oss-cn-beijing.aliyuncs.com/img/avatar1.jpg"},
+                {id:4,src:"https://gaoziyuan.oss-cn-beijing.aliyuncs.com/img/avatar2.jpg"},
                 // {id:5,src:"http://gaoziyuan.oss-cn-beijing.aliyuncs.com/img/avatar3.jpg"},
                 // {id:6,src:"http://gaoziyuan.oss-cn-beijing.aliyuncs.com/img/avatar4.jpg"},
             ]
