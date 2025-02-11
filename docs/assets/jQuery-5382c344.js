796d5bb5ed2a72bfc839b628849e54d7b7731fec
gaoziyuan <ziyuan_gao@163.com>
2025-02-11 17:53:49 +0800

    update:更新构建命令，打包后结果进行细分

--- docs/assets/jQuery-5382c344.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as _,c as t}from"./index-26742e61.js";const c={setup(){}};function o(s,n,r,a,p,u){return _(),t("div",null,"jquery")}const l=e(c,[["render",o],["__scopeId","data-v-e15382c9"]]);export{l as default};
--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,23 @@ export default defineConfig({
     }
   },
   build:{
-    assetsInlineLimit:0
+    assetsInlineLimit:0,
+    rollupOptions:{
+      output:{
+        entryFileNames:"js/[name]-[hash].js",
+        chunkFileNames:"js/[name]-[hash].js",
+        assetFileNames(assetInfo){
+          const name = assetInfo.name;
+          if(name.endsWith('.css')){
+            return "css/[name]-[hash].css"
+          }
+          const imgExts = ['.png', '.jpg', '.jpeg', '.gif', '.svg', '.webp'];
+          if(imgExts.some(ext => name.endsWith(ext))){
+            return "img/[name]-[hash].[ext]"
+          }
+          return "assets/[name]-[hash].[ext]"
+        }
+      }
+    }
   }
 })
